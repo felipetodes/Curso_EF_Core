666bf8073908313c86f92190be0d34b7c255f544
Felipe Todeschini <todeschini1992@gmail.com>
2025-02-01 00:02:34 -0300

    instalação de pacotes

---
+++ EntityFrameworkCoreConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EntityFrameworkCoreConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
